0572f617781b65a436ad96dc446bf06b29cd699e
Jose Ojeda <joche.ojeda@bitframeworks.com>
2020-08-11 16:58:56 -0700

    Nuget update

--- XpoWebApiAspNet/Controllers/XpoWebApiController.cs
+++ XpoWebApiAspNet/Controllers/XpoWebApiController.cs
@@ -13,45 +13,8 @@ namespace XpoWebApiAspNet.Controllers
         {
 
         }
-        //public IHttpActionResult Post()
-        //{
-        //    return base.Post(null);
-        //}
-        //public override IHttpActionResult Post([FromBody] DataParameters value)
-        //{
-        //    return Ok("Hi from the server");
-        //    //return base.Post(value);
-        //}
-        //public async override Task<IHttpActionResult> Post()
-        //{
-
-
-           
-
-        //    try
-        //    {
-        //        //Task<IHttpActionResult> task = base.Post();
-        //        //return task;
-
-
-        //        var body = this.Request.Content;
-
-
-
-        //        IDataParameters parameters = await DeserializeFromStream(await body.ReadAsStreamAsync());
-
-        //        parameters.AdditionalValues.Add("DataStoreId", "001");
-        //        IDataResult content = this.DataStoreFunctionServer.ExecuteFunction(parameters);
-        //        return Ok(content);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        var test = ex.Message;
-        //        throw;
-        //    }
-
-
-        //}
+ 
+   
 
     }
 }
